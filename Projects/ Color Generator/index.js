7e42081e8b0376563f53cedfbda552375ef38a08
syash7202 <workspacesyash7202@gmail.com>
2023-09-13 14:00:54 +0530

    color code generator app

---
+++ Projects/ Color Generator/index.js
@@ -0,0 +1,11 @@
+const getColorCode = () => {
+  const randomNumber = Math.floor(Math.random() * 16777215);
+  const randomCode = "#" + randomNumber.toString(16);
+  console.log(randomNumber, randomCode);
+  const element = document.querySelector(".container");
+  element.style.backgroundColor = randomCode;
+  document.getElementById("color-code").innerText = randomCode;
+};
+getColorCode();
+
+document.getElementById("btn").addEventListener("click", getColorCode);
