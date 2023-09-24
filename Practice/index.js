12ed8b1f96461c24800fb2ae2fadaf0fa48543c8
syash7202 <workspacesyash7202@gmail.com>
2023-09-25 00:41:12 +0530

    practice for js

--- Practice/index.js
+++ Practice/index.js
@@ -8,3 +8,16 @@ console.log(myDate.toDateString());
 console.log(myDate.toISOString());
 console.log(myDate.toLocaleString());
 console.log(typeof myDate);
+
+// custom date
+
+let createDateNew = new Date(2023, 0, 17); //array starts from 0
+console.log(createDateNew.toLocaleDateString());
+let createDateNew2 = new Date("2023-1-17"); //array starts from 1
+console.log(createDateNew2.toLocaleDateString());
+
+let today = new Date();
+console.log(today.getMonth()); // gives getMonth
+console.log(today.getTime()); // gives time
+
+console.log(`${today.toLocaleDateString()} is date`);
