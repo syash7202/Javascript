a6a5864bd408208f6647b4eb7f08c277463f5ee1
syash7202 <workspacesyash7202@gmail.com>
2023-09-24 20:21:29 +0530

    practice for js

---
+++ Practice/index.js
@@ -0,0 +1,10 @@
+// Practice sheet for Javascript
+
+let myDate = new Date();
+console.log(myDate);
+
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
